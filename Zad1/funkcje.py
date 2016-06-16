1f0c7f7dd2cfc1866b06cc01ffceafb019574970
Gwozdo3 <Gwozdo3@interia.pl>
2016-06-16 20:45:40 +0200

    ..;
    ;.

---
+++ Zad1/funkcje.py
@@ -0,0 +1,5 @@
+#Lambdy - jednolinijkowe kody minifunkcje
+z=5
+print (lambda x,y,z=4 : x+y+z)(0,0,z)
+#    (lambda  argumenty :  dzialanie)  ( wartosci dla argumentow)
+#w lambdach mozemy dac wartosci domyslne
